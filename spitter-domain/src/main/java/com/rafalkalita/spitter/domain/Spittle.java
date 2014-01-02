c02fb2e5b82b30e53ee5386673fc16f0c57faf15
Rafal Kalita <rafal@rafalkalita.com>
2014-01-02 16:26:51 +0000

    SPIT-9 - Reducing boilerplate.

--- spitter-domain/src/main/java/com/rafalkalita/spitter/domain/Spittle.java
+++ spitter-domain/src/main/java/com/rafalkalita/spitter/domain/Spittle.java
@@ -2,6 +2,10 @@ package com.rafalkalita.spitter.domain;
 
 import java.util.Date;
 
+import static org.apache.commons.lang.builder.EqualsBuilder.reflectionEquals;
+import static org.apache.commons.lang.builder.HashCodeBuilder.reflectionHashCode;
+import static org.apache.commons.lang.builder.ToStringBuilder.reflectionToString;
+
 /**
  * User: rafalkalita
  * Date: 20/11/2013
@@ -55,35 +59,16 @@ public class Spittle {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Spittle)) return false;
-
-        Spittle spittle = (Spittle) o;
-
-        if (!id.equals(spittle.id)) return false;
-        if (message != null ? !message.equals(spittle.message) : spittle.message != null) return false;
-        if (spitter != null ? !spitter.equals(spittle.spitter) : spittle.spitter != null) return false;
-        if (whenCreated != null ? !whenCreated.equals(spittle.whenCreated) : spittle.whenCreated != null) return false;
-
-        return true;
+        return reflectionEquals(this, o);
     }
 
     @Override
     public int hashCode() {
-        int result = id.hashCode();
-        result = 31 * result + (spitter != null ? spitter.hashCode() : 0);
-        result = 31 * result + (message != null ? message.hashCode() : 0);
-        result = 31 * result + (whenCreated != null ? whenCreated.hashCode() : 0);
-        return result;
+        return reflectionHashCode(this);
     }
 
     @Override
     public String toString() {
-        return "Spittle{" +
-                "id=" + id +
-                ", spitter=" + spitter +
-                ", message='" + message + '\'' +
-                ", whenCerated=" + whenCreated +
-                '}';
+        return reflectionToString(this);
     }
 }
