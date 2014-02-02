94b39d66e33e33a23a09a45eccbc2f91c7f10268
rafalkalita <rafal@rafalkalita.com>
2014-02-02 23:52:54 +0000

    SPIT-15 - I want to be able to register a new user JSR-303 Validation.

--- spitter-model/src/main/java/com/rafalkalita/spitter/model/Spitter.java
+++ spitter-model/src/main/java/com/rafalkalita/spitter/model/Spitter.java
@@ -1,6 +1,8 @@
 package com.rafalkalita.spitter.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import static org.apache.commons.lang.builder.EqualsBuilder.reflectionEquals;
 import static org.apache.commons.lang.builder.HashCodeBuilder.reflectionHashCode;
@@ -17,8 +19,14 @@ public class Spitter {
 
     private Long id;
 
+    @Size(min=3, max=20, message="Username must be between 3 and 20 characters long.")
+    @Pattern(regexp="^[a-zA-Z0-9]+$", message="Username must be alphanumeric with no spaces")
     private String username;
+
+    @Size(min=6, max=20, message="The password must be at least 6 characters long.")
     private String password;
+
+    @Size(min=3, max=50, message="Your full name must be between 3 and 50 characters long.")
     private String fullName;
 
     @Id
